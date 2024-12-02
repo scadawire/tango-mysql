6d627897634635505551f0f659df9840ebbed88b
Sebastian Jennen <sj@imagearts.de>
2024-12-02 12:51:08 +0100

    add warning/alarm handling

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    name="SQLServer",
+    name="Mysql",
     version="0.1.0",
     author="Sebastian Jennen",
     author_email="sj@imagearts.de",
-    description="SQLServer device driver",
+    description="Mysql device driver",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     install_requires=['requests'],
-    scripts=['SQLServer.py']
+    scripts=['Mysql.py']
 )
